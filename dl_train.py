bef0f321cd7ed9f507681c0c0e314a13aa608b5a
HeMuling <74801533+HeMuling@users.noreply.github.com>
2022-12-17 21:09:32 +0800

    2022.12.17, an update for pretrained model

--- dl_train.py
+++ dl_train.py
@@ -6,14 +6,13 @@ def init_weights(m):
     if type(m) == nn.Linear or type(m) == nn.Conv2d:
         nn.init.xavier_uniform_(m.weight)
 
-def dl_train(net, trainer, train_iter, test_iter, num_epoch, device=None):
-    '''
-    code adapted from https://www.zhihu.com/question/406133826/answer/1334319004
-    '''
+def dl_train(net,trainer , train_iter, test_iter, num_epoch, device=None, pre_trained=False):
 
     device = torch.device('cpu') if device == None else device
 
-    net.apply(init_weights)
+    if pre_trained == False:
+        net.apply(init_weights)
+    
     net.to(device)
 
     loss_function = nn.CrossEntropyLoss()
@@ -23,54 +22,40 @@ def dl_train(net, trainer, train_iter, test_iter, num_epoch, device=None):
     for epoch in range(num_epoch):
         running_loss = 0.0
         for x, y in enumerate(train_iter):
-            
-            # get data and label, send them into training device (e.g. cuda or cpu)
             data, label = y
             data = data.to(device)
             label = label.to(device)
 
-            # set the grad to zero
             trainer.zero_grad()
 
-            # forward prop
             y_hat = net(data)
             loss = loss_function(y_hat, label)
-
-            # backward prop
             loss.backward()
 
-            # update
             trainer.step()
 
             running_loss += loss.item()
-            
-            # show loss every 100 batches
             if x % 100 == 99:
                 print(f'epoch {epoch+1}, batch {x+1}, loss {running_loss/100:.3f}')
                 running_loss = 0.0
-    # begin evaluation
-    net.eval()
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for x, y in enumerate(test_iter):
+    
+    if test_iter != None:
+        # begin evaluation
+        net.eval()
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for x, y in enumerate(test_iter):
+                data, label = y
+                data = data.to(device)
+                label = label.to(device)
 
-            # get data and label, send them into training device (e.g. cuda or cpu)
-            data, label = y
-            data = data.to(device)
-            label = label.to(device)
-            
-            # compute the prediction
-            y_hat = net(data)
+                y_hat = net(data)
+                _, predicted = torch.max(y_hat.data, 1)
+                total += label.size(0)
+                correct += (predicted == label).sum().item()
 
-            # make the prediction
-            _, predicted = torch.max(y_hat.data, 1)
-
-            # make the count
-            total += label.size(0)
-            correct += (predicted == label).sum().item()
-
-        print(f'Accuracy of the network on the {total} test images: {100 * correct / total}%')
+            print(f'Accuracy of the network on the {total} test images: {100 * correct / total}%')
         
 
 '''
